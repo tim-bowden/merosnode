d6ca91839f5a528d387ab4aff16be3bdb4ea866b
tim-bowden <Administrator@HMTDEV.LOCAL>
2017-01-11 09:54:29 +0000

    Commiting Server

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict'
 
-var express = require('express');
+const express = require('express');
 
-var app = express.
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
